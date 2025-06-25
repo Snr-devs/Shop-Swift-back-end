7844b9bef8eb667119f48276f3479cd1f9f07e48
chiya morara <chiyamorara@gmail.com>
2025-06-25 17:16:27 +0300

    edited some importations and manage.py

--- server/extensions.py
+++ server/extensions.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_JWT_extended import JWTManager
+from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_limiter import Limiter
-from flask_limiter_util import get_remote_address
+from flask_limiter.util import get_remote_address
 
 db= SQLAlchemy()
 migrate=Migrate()
 jwt=JWTManager()
 ma=Marshmallow()
-limiter=Limiter()
+limiter=Limiter(key_func=get_remote_address)
